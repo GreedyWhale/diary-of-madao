e112aefe7f51a3a10427773dba380af05064562f
caisr <383911973@qq.com>
2021-06-11 15:45:12 +0800

    feat: 添加网站icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,30 @@
  * @Author: MADAO
  * @Date: 2021-06-10 15:46:14
  * @LastEditors: MADAO
- * @LastEditTime: 2021-06-11 11:31:40
+ * @LastEditTime: 2021-06-11 15:44:18
  * @Description: 入口文件
  */
 import type {AppProps} from 'next/app';
+import Head from 'next/head';
 import 'styles/globals.scss';
 
 function MyApp({Component, pageProps}: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>MADAO观察日记</title>
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+				<link rel="icon" href="/favicon.ico" />
+				<link rel="manifest" href="/site.webmanifest" />
+				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f05454" />
+				<meta name="msapplication-TileColor" content="#f05454" />
+				<meta name="theme-color" content="#f05454" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
