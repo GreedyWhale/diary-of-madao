615831bd69b0c3e6d23d3ca17f834ae6674950a4
caisr <383911973@qq.com>
2021-06-11 18:19:42 +0800

    feat: 首页标题栏样式完成40%

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,13 @@
  * @Author: MADAO
  * @Date: 2021-06-10 15:46:14
  * @LastEditors: MADAO
- * @LastEditTime: 2021-06-11 15:44:18
+ * @LastEditTime: 2021-06-11 18:10:28
  * @Description: 入口文件
  */
 import type {AppProps} from 'next/app';
 import Head from 'next/head';
-import 'styles/globals.scss';
+import '~/styles/globals.scss';
+import styles from '~/styles/layout.module.scss';
 
 function MyApp({Component, pageProps}: AppProps) {
 	return (
@@ -23,7 +24,15 @@ function MyApp({Component, pageProps}: AppProps) {
 				<meta name="msapplication-TileColor" content="#f05454" />
 				<meta name="theme-color" content="#f05454" />
 			</Head>
-			<Component {...pageProps} />
+			<main className={styles.layout} data-theme="purple">
+				<div className={styles.header}>
+					<h1 className={styles.title}>
+						<a href="/">MADAO观察日记</a>
+					</h1>
+					<div className={styles.dividingLine} />
+				</div>
+				<Component {...pageProps} />
+			</main>
 		</>
 	);
 }
