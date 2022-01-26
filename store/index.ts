7eb4a249681860829c0919d7d8cdeb8e95537e5e
Caisr <madaocai@gmail.com>
2022-01-26 10:16:12 +0800

    style: 优化代码格式

--- store/index.ts
+++ store/index.ts
@@ -3,21 +3,19 @@
  * @Author: MADAO
  * @Date: 2021-07-28 21:11:00
  * @LastEditors: MADAO
- * @LastEditTime: 2021-08-02 12:37:27
+ * @LastEditTime: 2022-01-26 10:15:53
  */
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 
 import userReducer from './slice/user';
 
-const createStore = () => {
-  return configureStore({
-    reducer: {
-      user: userReducer
-    },
-    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger)
-  });
-};
+const createStore = () => configureStore({
+  reducer: {
+    user: userReducer,
+  },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
+});
 
 export type RootState = ReturnType<ReturnType<typeof createStore>['getState']>;
 export type AppDispatch = ReturnType<typeof createStore>['dispatch'];
