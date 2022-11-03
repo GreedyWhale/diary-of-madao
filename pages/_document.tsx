efe9276d4c8b1c183e4b0d3ead19faa4848182fc
Caisr <madaocai@gmail.com>
2022-11-03 17:22:19 +0800

    feat: 增加网站icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <link rel='icon' type='image/png' href='/favicon.ico' />
         <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/lxgw-wenkai-webfont@1.6.0/style.css' />
         <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/lxgw-wenkai-screen-webfont@1.1.0/style.css' />
       </Head>
