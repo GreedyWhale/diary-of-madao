e639a8fc88792f1af80eab95b5df7500a4e152e1
Caisr <madaocai@gmail.com>
2022-11-16 22:56:44 +0800

    fix: 修复git 同步失败问题

--- model/git.ts
+++ model/git.ts
@@ -3,7 +3,7 @@
  * @Author: MADAO
  * @Date: 2022-10-22 16:01:23
  * @LastEditors: MADAO
- * @LastEditTime: 2022-10-22 16:28:04
+ * @LastEditTime: 2022-11-16 22:55:01
  */
 import simpleGit from 'simple-git';
 import path from 'path';
@@ -14,13 +14,15 @@ class GitModel {
   git = simpleGit();
   async syncToGitHub() {
     return this.git
+      .addConfig('user.name', 'Caisr')
+      .addConfig('user.email', 'madaocai@gmail.com')
       .add([
         path.join(process.cwd(), './public/upload'),
         path.join(process.cwd(), './public/notes'),
       ])
       .commit('update：更新笔记相关文件')
-      .pull('origin', 'refactor')
-      .push(['origin', 'refactor'])
+      .pull('origin', 'main')
+      .push(['origin', 'main'])
       .then(
         () => formatResponse({ status: 200, message: '同步完成' }),
         error => formatResponse({ status: 500, message: (error as Error).message }),
